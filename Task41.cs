0008112d384214527f074433e76a5db6b71c07d1
YaroslavDubV <yaroslavduboshin@mail.ru>
2022-08-27 20:38:11 +0300

    Task41

---
+++ Task41.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите кол-во элементов массива: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [i];
+int sum = 0;
+for (int count = 0; count < i; count++) {
+    Console.WriteLine($"Введите {count + 1} элемент массива");
+    array[count] = Convert.ToInt32(Console.ReadLine());
+    if (array[count] > 0) {
+        sum++; 
+    }
+}
+Console.WriteLine(string.Join (", ", array));
+Console.WriteLine($"Колличество выеденных чисел больше 0 = {sum}");
